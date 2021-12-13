ab81c73a6b289c27a8d9beafa76789d1ddaef30e
kschoi <kyungsik0907@gmail.com>
2021-12-13 15:08:55 +0900

    Please,Wake up Message
    - Application start

---
+++ src/main/java/com/meaningfullife/pwum/PwumApplication.java
@@ -0,0 +1,13 @@
+package com.meaningfullife.pwum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwumApplication.class, args);
+	}
+
+}
